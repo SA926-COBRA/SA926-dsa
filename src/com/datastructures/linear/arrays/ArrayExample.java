7f0e9fdf2f2fd8bbc84cfb96ac6f4cfef5c67fb0
SA926-COBRA <sayanacar926@gmail.com>
2024-11-26 22:48:54 +0545

    Implement basic array example in java

---
+++ src/com/datastructures/linear/arrays/ArrayExample.java
@@ -0,0 +1,10 @@
+package com.datastructures.linear.arrays;
+
+import java.util.Arrays;
+
+public class ArrayExample {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30};
+        System.out.println(Arrays.toString(numbers));
+    }
+}
