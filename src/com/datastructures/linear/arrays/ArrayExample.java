c839e526a0feb6e6d7074ab832d329bf85be1087
SA926-COBRA <sayanacar926@gmail.com>
2024-11-26 22:50:12 +0545

    Update ArrayExample: Print array length

--- src/com/datastructures/linear/arrays/ArrayExample.java
+++ src/com/datastructures/linear/arrays/ArrayExample.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class ArrayExample {
     public static void main(String[] args) {
         int[] numbers = {10, 20, 30};
+        System.out.println(numbers.length);
         System.out.println(Arrays.toString(numbers));
     }
 }
