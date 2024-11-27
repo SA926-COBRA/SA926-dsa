d87fd677291c13a613c1ada861af8f29c6bb4b7e
SA926-COBRA <sayanacar926@gmail.com>
2024-11-27 23:57:09 +0545

    call insert method to Main class

--- src/com/datastructures/linear/arrays/dynamic/Main.java
+++ src/com/datastructures/linear/arrays/dynamic/Main.java
@@ -3,6 +3,8 @@ package com.datastructures.linear.arrays.dynamic;
 public class Main {
     public static void main(String[] args) {
         Array numbers = new Array(3);
+        numbers.insert(10);
+        numbers.insert(20);
         numbers.print();
     }
 }
