88473603b918637b9c1c88e1906241366e104b19
SA926-COBRA <sayanacar926@gmail.com>
2024-11-27 23:56:02 +0545

    implement inset method

--- src/com/datastructures/linear/arrays/dynamic/Array.java
+++ src/com/datastructures/linear/arrays/dynamic/Array.java
@@ -9,6 +9,12 @@ public class Array {
         items = new int[length];
     }
 
+    public void insert(int item) {
+        // If the array is full, resize it
+        // Add new item at the end
+        items[count++] = item;
+    }
+
     public void print() {
         for (int i = 0; i < count; i++) {
             System.out.println(items[i]);
