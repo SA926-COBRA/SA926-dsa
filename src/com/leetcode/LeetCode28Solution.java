cffbbf826157045f324a273bb0e967c38186acfd
SA926-COBRA <sayanacar926@gmail.com>
2024-11-28 00:00:30 +0545

    LeetCode 28 solution

---
+++ src/com/leetcode/LeetCode28Solution.java
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=28 lang=java
+ *
+ * [28] Find the Index of the First Occurrence in a String
+ */
+
+// @lc code=start
+class Solution {
+    public int strStr(String haystack, String needle) {
+        int m = haystack.length(), n = needle.length();
+        for (int i = 0; i <= m - n; i++) {
+            if (haystack.substring(i, i + n).equals(needle)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+// @lc code=end
+
